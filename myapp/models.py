5248d96d97413d0d12cbe614edb693295767e504
sudhakar <sudhakarillamca@gmail.com>
2025-03-05 17:49:33 +0530

    my app

---
+++ myapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Employee(models.Model):
+   eid = models.CharField(max_length=20)
+   ename = models.CharField(max_length=120)
+   econtact = models.CharField(max_length=20)
+   class Meta:
+    db_table = "employee"
+    
+
+# Create your models here.
